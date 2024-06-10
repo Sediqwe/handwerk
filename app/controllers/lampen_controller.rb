214cf9a918b19f3f0a7b35ed32e8e89234c521ca
Sediqwe <sediqwe@gmail.com>
2024-06-10 21:37:45 +0200

    Fix

--- app/controllers/lampen_controller.rb
+++ app/controllers/lampen_controller.rb
@@ -1,6 +1,6 @@
 class LampenController < ApplicationController
   def index
     @felveszem = Felveszem.new
-    @lampen = Felveszem.where(link: "lampen").order(sorrend: :DESC)
+    @lampen = Felveszem.where(link: "lampen").order(sorrend: :ASC)
   end
 end
