c59159a9959e10f5daa5dc4d1bfdde68b9be7025
Sediqwe <sediqwe@gmail.com>
2024-06-10 20:25:21 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'lampen',to : "lampen#index"
+  get 'lampen',to: "lampen#index"
   resources :felveszems
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "lampen#index"
+  root "lampeng#index"
 end
