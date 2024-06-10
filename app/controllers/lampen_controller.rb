43b9ebf10229b42f6233b78f7ea7fed3b6de2b7f
Sediqwe <sediqwe@gmail.com>
2024-06-10 21:33:26 +0200

    törlés

--- app/controllers/lampen_controller.rb
+++ app/controllers/lampen_controller.rb
@@ -1,6 +1,6 @@
 class LampenController < ApplicationController
   def index
     @felveszem = Felveszem.new
-    @lampen = Felveszem.where(link: "lampen")
+    @lampen = Felveszem.where(link: "lampen").order(sorrend: :DESC)
   end
 end
