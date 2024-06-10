ca4aa3625ad574df2663769bead3b07150f4764b
Sediqwe <sediqwe@gmail.com>
2024-06-10 21:47:46 +0200

    vissza

--- app/controllers/felveszems_controller.rb
+++ app/controllers/felveszems_controller.rb
@@ -51,7 +51,7 @@ class FelveszemsController < ApplicationController
     @felveszem.destroy
 
     respond_to do |format|
-      format.html { redirect_to felveszems_url, notice: "Felveszem was successfully destroyed." }
+      format.html { redirect_to request.referrer , notice: "Felveszem was successfully destroyed." }
       format.json { head :no_content }
     end
   end
